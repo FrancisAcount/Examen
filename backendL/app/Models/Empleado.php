c3b2aa06504e4513fd04af2a4d1aa0dd59ab4216
FRANZ <francisacoun009@gmail.com>
2025-10-27 22:00:38 -0400

    crear el proyecto anular y componente empleados

--- backendL/app/Models/Empleado.php
+++ backendL/app/Models/Empleado.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Empleado extends Model
 {
-    use HasFactory;
+    //use HasFactory;
 
      protected $fillable = [ 'nombre','apellido','correo','salario'];
 }
---
+++ backendL/routes/api.php
@@ -0,0 +1,4 @@
+<?php
+use App\Http\Controllers\EmpleadoController;
+Route::get('/empleados', [EmpleadoController::class, 'index']);
+Route::post('/empleados', [EmpleadoController::class, 'store']);
